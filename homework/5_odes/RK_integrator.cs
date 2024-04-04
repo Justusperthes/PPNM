14ffada858abd8a8ff82a1f3d1f4fe8ad2057d5c
Justusperthes <jacob.hoey@gmail.com>
2024-04-04 12:14:48 +0200

    worked on h5, got nowhere

--- homework/5_odes/RK_integrator.cs
+++ homework/5_odes/RK_integrator.cs
@@ -1,26 +1,26 @@
 using System;
 using static System.Math;
-public class RK_integrator{
-    private Func<double,vector,vector> f;
+public static class RK_integrator{
+    /* private Func<double,vector,vector> f;
     private double x;
     private vector y;
-    private double h;
+    private double h; */
 
-    public RK_integrator(Func<double,vector,vector> f,double x,vector y,double h){
-        this.f = f; /* the f from dy/dx=f(x,y) */
-        this.x = x; /* the current value of the variable */
-        this.y = y; /* the current value y(x) of the sought function */
-        this.h = h; /* the step to be taken */
-    }
-    public (vector, vector) RKstep12(Func<double,vector,vector> f,double x,vector y,double h){
+    //public RK_integrator(Func<double,vector,vector> f,double x,vector y,double h){
+    //    this.f = f; /* the f from dy/dx=f(x,y) */
+    //    this.x = x; /* the current value of the variable */
+    //    this.y = y; /* the current value y(x) of the sought function */
+    //    this.h = h; /* the step to be taken */
+    //}
+    public static (vector, vector) RKstep12(Func<double,vector,vector> f,double x,vector y,double h){
         vector k0 = f(x,y);              /* embedded lower order formula (Euler) */
         vector k1 = f(x+h/2,y+k0*(h/2)); /* higher order formula (midpoint) */
         vector yh = y+k1*h;              /* y(x+h) estimate */
         vector δy = (k1-k0)*h;           /* error estimate */
         return (yh,δy);
     }//stepper
-    public (genlist<double>,genlist<vector>) driver(
-        Func<double,vector,vector> F,/* the f from dy/dx=f(x,y) */
+    public static (genlist<double>,genlist<vector>) driver(
+        Func<double,vector,vector> f,/* the f from dy/dx=f(x,y) */
         (double,double) interval,    /* (start-point,end-point) */
         vector ystart,               /* y(start-point) */
         double h,              /* initial step-size */
@@ -73,5 +73,4 @@ public class RK_integrator{
             }
         }
     }
-
 }
--- homework/5_odes/main.cs
+++ homework/5_odes/main.cs
@@ -1,20 +1,33 @@
 using System;
 class main{
     static void Main(){
-        Func<double, vector, vector> f = (double x, vector y) => {
+        Func<double, vector, vector> f = (double x, vector y) =>
+        {
+            double dydx = -x;
+
+            return new vector(dydx);
+        }; 
+        var yy = new vector(1);
+        var (xlist,ylist) = RK_integrator.driver(f,(0,10),yy,h:0.125,acc:0.01,eps:0.01);
+	    for(int i=0;i<xlist.size;i++){
+		    System.Console.WriteLine($"main: result: {xlist[i]} {ylist[i][0]}");
+        }
+    }
+}
+
+
+
+
+/* Func<double, vector, vector> f = (x, y) =>
+        {
+            double dy1dx = y[1];
+            double dy2dx = -y[0];
+
+            return new vector(dy1dx, dy2dx);
+        }; */
+
+
+        /* Func<double, vector, vector> f = (double x, vector y) => {
             double dydx = x; 
             return new vector(dydx);
-        };
-        double xx = 1;
-        var yy = new vector(1);
-        double h = 0.1;
-        RK_integrator my_RK = new RK_integrator(f,xx,yy,h);
-        var (xlist,ylist) = my_RK.driver(f,(0,10),yy,h:0.125,acc:0.01,eps:0.01);
-	for(int i=0;i<xlist.size;i++)
-		System.Console.WriteLine($"main: result: {xlist[i]} {ylist[i][0]}");
-        //var item1 = result.Item1;
-        //item1.Print();
-        /* var item2 = result.Item2;
-        item2.Print(); */
-    }
-} 
+        };  */
