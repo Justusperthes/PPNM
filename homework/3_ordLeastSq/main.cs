fa4aea03c877bfc2e63372eaba36c5dd549ead5c
Justusperthes <jacob.hoey@gmail.com>
2024-04-25 08:46:21 +0200

    h3 refactoring, but doesnt work anymore thoguh

--- homework/3_ordLeastSq/main.cs
+++ homework/3_ordLeastSq/main.cs
@@ -20,67 +20,15 @@ class main
         double a = Exp(fittingResult.Item1[0]); // Extracting 'a' from the intercept
         double lambda = -fittingResult.Item1[1]; // Extracting 'lambda' from the slope
     
-        // Write data to a file
-        using (System.IO.StreamWriter file = new System.IO.StreamWriter("data.txt"))
-        {
-            for (double x = 0; x <= 15; x += 0.1)
-            {
-                double y = a * Exp(-lambda * x);
-                file.WriteLine($"{x} {y}");
-            }
-        }
- 
-        // Create a Gnuplot script for both fit and data points with error bars
-        string plotScript = @"
-            set terminal pngcairo enhanced font 'arial,10' size 800,600
-            set output 'plot.png'
-            set xlabel 't (days)'
-            set ylabel 'activity'
-            plot 'data.txt' with lines title 'Exponential Fit', '-' with yerrorbars title 'Data Points'
-        ";
-
-        // Append data and errors to the script
-        for (int i = 0; i < X.size; i++)
-        {
-            plotScript += $"{X[i]} {Y[i]} {DY[i]}\n";
-        }
-
-        // Append 'e' to signify the end of data
-        plotScript += "e";
-
-        plotter.ExecuteGnuplotScript(plotScript);
+        // Write data to a file and run Gnuplot script
+        plotter.writeDataToFile(lambda,a);
+        string plotScript = plotter.createGnuplotScript();
+        plotter.runPlotScript(X,Y,DY,plotScript);
 
         //Half-life
         double T_half = Log(2)/lambda;
         Console.WriteLine($"The half-life is T_1/2 = {T_half.ToString("F" + 2)} d, compared with " +
         "the modern value of 3.6319 d.");
-    }
-    static vector CalculateNaturalLog(vector data)
-    {
-        vector lnData = new vector(data.size);
-        for (int i = 0; i < data.size; i++)
-        {
-            lnData[i] = Log(data[i]);
-        }
-        return lnData;
-    }
-    public static (vector, matrix, vector) PerformLinearLeastSquaresFit(vector X, vector Y, vector DY)
-    {
-        // Fitting to linear combination of {1,x}
-        Func<double, double>[] fs = new Func<double, double>[]
-        {
-            x => 1,
-            x => x
-        };
-
-        return ordLeastSq.lsfit(fs, X, Y, DY);
-    }
-    public static (vector, matrix, vector) PerformRootFit(vector X, vector Y, vector DY)
-    {
-        // for future development...
-        vector x = new vector(1);
-        matrix y = new matrix(2);
-        vector dy = new vector(3);
-        return (x,y,dy);
-    }
+    } 
+    
 }
--- homework/3_ordLeastSq/plotter.cs
+++ homework/3_ordLeastSq/plotter.cs
@@ -21,4 +21,64 @@ public static class plotter{
                 process.WaitForExit();
             }
         }
+    public static void writeDataToFile(double lambda, double a){
+        using (System.IO.StreamWriter file = new System.IO.StreamWriter("data.txt"))
+        {
+            for (double x = 0; x <= 15; x += 0.1)
+            {
+                double y = a * Exp(-lambda * x);
+                file.WriteLine($"{x} {y}");
+            }
+        }
+    }
+    public static string createGnuplotScript(){
+        string plotScript = @"
+            set terminal pngcairo enhanced font 'arial,10' size 800,600
+            set output 'plot.png'
+            set xlabel 't (days)'
+            set ylabel 'activity'
+            plot 'data.txt' with lines title 'Exponential Fit', '-' with yerrorbars title 'Data Points'
+        ";
+        return plotScript;
+    }
+    public static void runPlotScript(vector X, vector Y, vector DY, string plotScript){
+        // Append data and errors to the script
+        for (int i = 0; i < X.size; i++) 
+        {
+            plotScript += $"{X[i]} {Y[i]} {DY[i]}\n";
+        } 
+
+        // Append 'e' to signify the end of data
+        plotScript += "e";
+
+        ExecuteGnuplotScript(plotScript);
+    }
+    public static vector CalculateNaturalLog(vector data)
+    {
+        vector lnData = new vector(data.size);
+        for (int i = 0; i < data.size; i++)
+        {
+            lnData[i] = Log(data[i]);
+        }
+        return lnData;
+    }
+    public static (vector, matrix, vector) PerformLinearLeastSquaresFit(vector X, vector Y, vector DY)
+    {
+        // Fitting to linear combination of {1,x}
+        Func<double, double>[] fs = new Func<double, double>[]
+        { 
+            x => 1,
+            x => x
+        };
+
+        return ordLeastSq.lsfit(fs, X, Y, DY);
+    }
+    public static (vector, matrix, vector) PerformRootFit(vector X, vector Y, vector DY)
+    {
+        // for future development...
+        vector x = new vector(1);
+        matrix y = new matrix(2);
+        vector dy = new vector(3);
+        return (x,y,dy);
+    }
 }
