6631f6fbdbdbb9349c7e354ef207959243d64f87
Justusperthes <jacob.hoey@gmail.com>
2024-05-06 11:49:21 +0200

    refactored h8 using common classes

--- homework/9_minimisation/main.cs
+++ homework/9_minimisation/main.cs
@@ -32,14 +32,14 @@ public static class main{
             {
                 x = v[0];
                 y = v[1];
-                f = Math.Pow((x*x+y-11),2)+Math.Pow((x+y*y-7),2); //Math.Pow((1-x),2)+100*Math.Pow((y-Math.Pow(x,2)),2);
+                f = Math.Pow((x*x+y-11),2)+Math.Pow((x+y*y-7),2);
                 return f;
             };
         startingPoint = new vector(1.5,1.5);
         (result, steps) = Minimisation.Newton(Himmelblau, startingPoint);
         Console.WriteLine("Solution found at (x,y) = ");
         result.print();
-        objectiveValue = Rosenbrock(result);
+        objectiveValue = Himmelblau(result);
         Console.WriteLine("Value of f(x,y) at the minimum: " + objectiveValue);
         Console.WriteLine($"Completed in {steps} steps.");
     }
