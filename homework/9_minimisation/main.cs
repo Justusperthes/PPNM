13c7903952d878782b7b5fa50cf21053ff47cabd
Justusperthes <jacob.hoey@gmail.com>
2024-06-18 19:26:13 +0200

    refactored all the homeworks to work with common classes; updated h9 main method to find all 4 minima of Himmelblau

--- homework/9_minimisation/main.cs
+++ homework/9_minimisation/main.cs
@@ -19,9 +19,10 @@ public static class main{
                 f = Math.Pow((1-x),2)+100*Math.Pow((y-Math.Pow(x,2)),2);
                 return f;
             };
+        //find minimum of Rosenbrock fct
         startingPoint = new vector(1.5, 1.5);
         (result, steps) = Minimisation.Newton(Rosenbrock, startingPoint);
-        Console.WriteLine("Solution found at (x,y) = ");
+        Console.WriteLine("Rosenbrock function has minimum at (x,y) = ");
         result.print();
         objectiveValue = Rosenbrock(result);
         Console.WriteLine("Value of f(x,y) at the minimum: " + objectiveValue);
@@ -35,9 +36,24 @@ public static class main{
                 f = Math.Pow((x*x+y-11),2)+Math.Pow((x+y*y-7),2);
                 return f;
             };
-        startingPoint = new vector(1.5,1.5);
+        double[] xs = {-6.0, 6.0};
+        double[] ys = {-6.0, 6.0};
+        //Find minima of Himmelblau fct
+        for (int i = 0; i < 2; i++){
+            for (int j = 0; j < 2; j++){
+                startingPoint = new vector(xs[i],ys[j]);
+                (result, steps) = Minimisation.Newton(Himmelblau, startingPoint);
+                Console.WriteLine("Himmelblau function has minimum at (x,y) = ");
+                result.print();
+                objectiveValue = Himmelblau(result);
+                Console.WriteLine("Value of f(x,y) at the minimum: " + objectiveValue);
+                Console.WriteLine($"Completed in {steps} steps.");
+            }
+        }
+        //Find minimum 2
+        startingPoint = new vector(-5.0,-5.0);
         (result, steps) = Minimisation.Newton(Himmelblau, startingPoint);
-        Console.WriteLine("Solution found at (x,y) = ");
+        Console.WriteLine("Himmelblau function has minimum at (x,y) = ");
         result.print();
         objectiveValue = Himmelblau(result);
         Console.WriteLine("Value of f(x,y) at the minimum: " + objectiveValue);
